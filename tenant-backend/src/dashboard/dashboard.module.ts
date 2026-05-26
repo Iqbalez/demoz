30e390d163e9e477baa166d2f100ee1072477936
Iqbalez <iqbalezedin5@gmail.com>
2026-05-27 00:06:11 +0300

    Fix DashboardModule PrismaService dependency

--- tenant-backend/src/dashboard/dashboard.module.ts
+++ tenant-backend/src/dashboard/dashboard.module.ts
@@ -1,11 +1,12 @@
 import { Module, Global } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 
 @Global()
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService],
+  providers: [DashboardService, PrismaService],
   exports: [DashboardService],
 })
 export class DashboardModule {}
